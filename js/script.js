d07a0bd689f93f26fc1cd68e371ea10869d337f0
haileyesus <teamdevs7@gmail.com>
2024-01-29 16:05:09 +0000

    slider added

--- js/script.js
+++ js/script.js
@@ -84,6 +84,47 @@ const displayTvShow = async () => {
   console.log(results)
 }
 
+//slider show
+
+// const sliderShow = async () => {
+//   const { results } = await fetchApi("movie/now_playing")
+//   results.forEach((movie) => {
+//     const div = document.createElement("div")
+//     div.classList.add("swiper-slide")
+
+//     div.innerHTML = `
+//             <a href="movie-details.html?id=${movie.id}">
+//               <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+//             </a>
+//             <h4 class="swiper-rating">
+//               <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+//             </h4>
+//          `
+//     document.querySelector(".swiper-wrapper").appendChild(div)
+
+//     initSwiper()
+//   })
+// }
+// // init swiper
+// function initSwiper() {
+//   const swiper = new Swiper(".swiper", {
+//     slidesPreView: 1,
+//     spaceBetween: 30,
+//     freeMode: true,
+//     autoPlay: {
+//       delay: 4000,
+//       disableOnInteraction: false,
+//     },
+//     breakpoints: {
+//       500: { slidesPreView: 2 },
+//       700: {
+//         slidesPreView: 3,
+//       },
+//       1200: { slidesPreView: 4 },
+//     },
+//   })
+// }
+
 //display movie detail
 
 const displayMovieDetail = async () => {
@@ -267,6 +308,7 @@ const init = () => {
   switch (global.currnetPage) {
     case "/":
     case "/index.html":
+      // sliderShow()
       displayPopularMovies()
       break
     case "/movie-details.html":
